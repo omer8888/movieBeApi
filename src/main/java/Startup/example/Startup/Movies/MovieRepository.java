00a4493923db361dab4ca100d1078cbcf031ac1a
omer <omernaim8@gmail.com>
2024-05-13 13:47:33 +0300

    support the FE access

--- src/main/java/Startup/example/Startup/Movies/MovieRepository.java
+++ src/main/java/Startup/example/Startup/Movies/MovieRepository.java
@@ -2,7 +2,6 @@ package Startup.example.Startup.Movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 
-    @Query()
     public Optional<Movie> findMovieByImdbId(String imdbId);
 
 }
